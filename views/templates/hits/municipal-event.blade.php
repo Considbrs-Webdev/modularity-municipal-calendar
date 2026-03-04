9943018a62e20a5d26da91c1c9031181b48c8a65
William Lundqvist <william.lundqvist@consid.se>
2026-03-04 16:10:54 +0100

    Update municipal event card view to use badge for subheading
    - Replaced the subheading span with a badge component for improved styling and consistency in the municipal event view.

--- views/templates/hits/municipal-event.blade.php
+++ views/templates/hits/municipal-event.blade.php
@@ -11,14 +11,7 @@
                 <div class="c-group c-group--vertical c-group--gap-1">
                     {{-- Metadata row: time range, separator dot, place --}}
                     <div class="c-group c-group--horizontal c-group--align-items-center c-group--gap-1">
-                        <span class="c-typography c-card__sub-heading u-margin__y--0 c-typography__variant--h6">
-                            {SEARCH_HIT_SUBHEADING}
-                        </span>
-                        <span class="u-color__text--primary u-display--inline-flex u-align-items--center" aria-hidden="true">
-                            <svg width="8" height="8" viewBox="0 0 8 8" fill="currentColor" aria-hidden="true">
-                                <circle cx="4" cy="4" r="4" />
-                            </svg>
-                        </span>
+                        <span class="c-badge c-badge--primary">{SEARCH_HIT_SUBHEADING}</span>
                         <span class="c-typography">
                             {SEARCH_HIT_TIME_RANGE}
                         </span>
